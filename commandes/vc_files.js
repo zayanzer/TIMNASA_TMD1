b684ae0f9ff9b50a0466a6db01dec55f09f68bcc
Next5x <botuse41@gmail.com>
2024-12-26 10:17:39 +0300

    Initial commit

---
+++ commandes/vc_files.js
@@ -0,0 +1,235 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../framework/dl/Function");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
+
+zokou({ nomCom: 'vcf1',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '📄', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_sorry just use 👉 vcf10 to get all contact_*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'getallmembers',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '💀', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_getting all members_*`) 
+
+   
+
+
+  }
+);
+
+
+
+zokou({ nomCom: 'channel',
+    desc: 'To check runtime',
+    Categorie: 'My Contact',
+    reaction: '🎁', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`Support Here My Owner By Follow This Channel Please :https://whatsapp.com/channel/0029VagWQ255q08VTCRQKP09) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'group1',
+    desc: 'To check runtime',
+    Categorie: 'My Contact',
+    reaction: '😂', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*Tap Here To Join 𝐀𝐍𝐘𝐖𝐀𝐘 𝐌𝐃 Chatroom* https://chat.whatsapp.com/GUM0wglBlfo9WxCYWxEUJ9) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'update',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '⚙️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_𝐀𝐍𝐘𝐖𝐀𝐘 𝐌𝐃🎁is running on its latest vision_*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'vision',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🔎', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_𝐀𝐍𝐘𝐖𝐀𝐘 𝐌𝐃_*`) 
+
+   
+
+
+  }
+);
+
+
+  
+zokou({ nomCom: 'cheka',
+    desc: 'To check runtime',
+    Categorie: 'My Contact',
+    reaction: '😂', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_tap the link below to join CHEKA CHANNEL= https://whatsapp.com/channel/0029VafooOgAjPXNXqBpW01X_*`) 
+
+   
+
+
+  }
+)
+
+
+zokou({ nomCom: 'hack2',
+    desc: 'To check runtime',
+    Categorie: 'My Contact',
+    reaction: '5XN238AE😎', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`Injecting Malware",
+    " █ 10%",
+    " █ █ 20%",
+    " █ █ █ 30%",
+    " █ █ █ █ 40%",
+    " █ █ █ █ █ 50%",
+    " █ █ █ █ █ █ 60%",
+    " █ █ █ █ █ █ █ 70%",
+    " █ █ █ █ █ █ █ █ 80%",
+    " █ █ █ █ █ █ █ █ █ 90%",
+    " █ █ █ █ █ █ █ █ █ █ 100%",
+    "System hijacking on process..\nConnecting to Server error to find 404",
+    "Device successfully connected...\nReceiving data...",
+    "Data hijacked from device 100% completed\nKilling all evidence, killing all malwares...",
+    "HACKING COMPLETED",
+    "SENDING LOG DOCUMENTS...",
+    "SUCCESSFULLY SENT DATA AND Connection disconnected",
+    "BACKLOGS CLEARED",
+    "POWERED BY 𝐀𝐍𝐘𝐖𝐀𝐘 𝐌𝐃",
+    "By 𝐀𝐍𝐘𝐖𝐀𝐘 𝐓𝐄𝐂𝐇`) 
+
+   
+
+
+  }
+)
+
+
+
+
+zokou({ nomCom: 'problem',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🔎', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_┏━━━━━━━━━━━━━━
+┃𝐀𝐍𝐘𝐖𝐀𝐘 𝐌𝐃 
+| 
+┃   
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❶ || Creator = 𖥘 𝐀𝐍𝐘𝐖𝐀𝐘 𝐌𝐃 𖥘
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❷ || https://whatsapp.com/channel/0029VagWQ255q08VTCRQKP09
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+Please Follow My YouTube Channel
+🌛 https://www.youtube.com/@Yesserboy92
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+©*2024-2099 𝐀𝐍𝐘𝐖𝐀𝐘 𝐓𝐄𝐂𝐇_*`) 
+
+   
+
+
+  }
+);
+
+
