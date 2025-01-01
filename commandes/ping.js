50c1cdb7942e87294883b71ec31884e04484cac9
Next5x <botuse41@gmail.com>
2025-01-01 17:34:19 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -6,7 +6,7 @@ zokou({ nomCom: "best", reaction: "🧒", nomFichier: __filename }, async (dest,
     let z = '*🌍𝑩𝒐𝒕 𝒊𝒔 𝒐𝒏𝒍𝒊𝒏𝒆🌍* 🙏 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
     let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
     let varmess = z + d;
-    var mp4 = 'https://i.imgur.com/8TOvg5C.jpeg';
+    var mp4 = 'https://file.io/b4MYJgIWwQUn';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
