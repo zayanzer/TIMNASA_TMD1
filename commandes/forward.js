d079de862090188b179214bdca933fcf4902faef
Next5x <botuse41@gmail.com>
2025-02-20 11:02:01 +0300

    Update forward.js

--- commandes/forward.js
+++ commandes/forward.js
@@ -23,7 +23,7 @@ zokou({ nomCom: "forward", categorie: 'Group', reaction: "📤" }, async (dest, 
       // Send the message directly to the member's DM
       await zk.sendMessage(memberNumber, { text: mess });
     }
-    repondre('*CASEYRHODES XMD Forwarded Your Message To All group members in their DMs.*');
+    repondre('*𝗧𝗜𝗠𝗡𝗔𝗦𝗔 𝗠𝗗 Forwarded Your Message To All group members in their DMs.*');
   } else {
     repondre('*Only My Owner Can Use This Command.*');
   }
